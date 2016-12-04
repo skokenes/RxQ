401adbae792bf8947ecf54d85de9f29ac0ff8d9c
Speros Kokenes <speroskokenes@gmail.com>
2016-12-04 11:18:37 -0500

    updated example to be es5, comments

--- examples/chart-with-filter/main.js
+++ examples/chart-with-filter/main.js
@@ -4,17 +4,16 @@ var config = {
     isSecure: false
 };
 
-// Observable that connects to the server
+// Observable that connects to the server and returns QixGlobal
 var eng$ = RxQ.connectEngine(config);
 
-// Observable that opens the Executive Dashboard
+// Observable that opens the Executive Dashboard QixApp and shares it
 var app$ = eng$
     .qOpenDoc("24703994-1515-4c2c-a785-d769a9226143")
     .publishReplay(1)
     .refCount();
 
-
-// GenericObject Def 
+// GenericObject definition for the chart
 var genObjProp = {
     qInfo: {
         qType: "chart"
@@ -48,34 +47,11 @@ var genObjProp = {
     }
 };
 
+// Create the generic object for the chart
 var gO$ = app$
     .qCreateSessionObject(genObjProp);
 
-var lbProp = {
-    "qInfo": {
-        "qType": "filter"
-    },
-    "qListObjectDef": {
-        "qDef": {
-            "qFieldDefs": [
-                "Product Group"
-            ]
-        },
-        "qInitialDataFetch": [
-            {
-                "qLeft": 0,
-                "qTop": 0,
-                "qWidth": 1,
-                "qHeight": 1000
-            }
-        ]
-    }
-};
-
-var lb$ = app$
-    .qCreateSessionObject(lbProp);
-
-// Set up a chart
+// Initialize the chart with empty data
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
@@ -93,20 +69,73 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero: true,
+                    callback: function(label){return  ' $' + Math.round(label).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");}
                 }
             }]
         }
     }
 });
 
+// Get an observable stream of generic object layouts when the data changes, and update the chart with latest data
 gO$
     .qLayouts()
-    .subscribe(l=>{
-        var data = l.response.qLayout.qHyperCube.qDataPages[0].qMatrix;
-        myChart.data.labels = data.map(d=>d[0].qText);
-        myChart.data.datasets[0].data = data.map(d=>d[1].qNum);
+    .subscribe(function(layout) {
+        var data = layout.response.qLayout.qHyperCube.qDataPages[0].qMatrix;
+        myChart.data.labels = data.map(function(d) { return  d[0].qText; });
+        myChart.data.datasets[0].data = data.map(function(d) { return d[1].qNum; });
         myChart.update();
     });
 
-lb$.subscribe(s=>console.log(s));
+// Define a generic object for the listbox
+var lbProp = {
+    "qInfo": {
+        "qType": "filter"
+    },
+    "qListObjectDef": {
+        "qDef": {
+            "qFieldDefs": [
+                "Product Group Desc"
+            ]
+        },
+        "qInitialDataFetch": [
+            {
+                "qLeft": 0,
+                "qTop": 0,
+                "qWidth": 1,
+                "qHeight": 1000
+            }
+        ]
+    }
+};
+
+// Create the listbox generic object
+var lb$ = app$
+    .qCreateSessionObject(lbProp);
+
+// Get listbox layouts and update the listbox UI
+var ul = document.getElementById("myListbox");
+var lbsub = lb$
+    .qLayouts()
+    .subscribe(function(layout) {
+        var data = layout.response.qLayout.qListObject.qDataPages[0].qMatrix;
+        ul.innerHTML = data
+            .map(function(m) { return "<li class='" +  m[0].qState + "' data-qelemno=" + m[0].qElemNumber + ">" + m[0].qText + "</li>"; })
+            .join("");
+    });
+
+// Create a stream of selection calls based on click events
+var select$ = Rx.Observable.fromEvent(ul,"click")
+    .withLatestFrom(lb$)
+    .map(function(vals) {
+        var evt = vals[0];
+        var lbObj = vals[1];
+        var elemNo = parseInt(evt.target.getAttribute("data-qelemno"));
+        return lbObj.SelectListObjectValues("/qListObjectDef",[elemNo],true)
+            .publish();
+    });
+
+// When selection call is created, execute it
+select$.subscribe(function(sel) {
+    sel.connect();
+});
